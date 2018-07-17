ba5a5b2c2edd21a7ae6c786c8e781915b9f83724
Nathan Vander Martin <natoalaska@gmail.com>
2018-07-17 09:26:24 -0800

    Created Templates
    Started build on some templates.

--- application/logs/log-2018-07-17.php
+++ application/logs/log-2018-07-17.php
@@ -1,1 +1,38 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+ERROR - 2018-07-17 09:05:52 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'cishop' C:\xampp\htdocs\cishop\system\database\drivers\mysqli\mysqli_driver.php 135
+ERROR - 2018-07-17 09:05:52 --> Unable to connect to the database
+ERROR - 2018-07-17 09:06:14 --> Severity: error --> Exception: Too few arguments to function Templates::public_bootstrap(), 0 passed in C:\xampp\htdocs\cishop\system\core\CodeIgniter.php on line 514 and exactly 1 expected C:\xampp\htdocs\cishop\application\modules\templates\controllers\templates.php 9
+ERROR - 2018-07-17 09:06:14 --> Severity: Error --> Uncaught TypeError: Argument 1 passed to CI_Exceptions::show_exception() must be an instance of Exception, instance of ArgumentCountError given, called in C:\xampp\htdocs\cishop\system\core\Common.php on line 658 and defined in C:\xampp\htdocs\cishop\system\core\Exceptions.php:190
+Stack trace:
+#0 C:\xampp\htdocs\cishop\system\core\Common.php(658): CI_Exceptions->show_exception(Object(ArgumentCountError))
+#1 [internal function]: _exception_handler(Object(ArgumentCountError))
+#2 {main}
+  thrown C:\xampp\htdocs\cishop\system\core\Exceptions.php 190
+ERROR - 2018-07-17 09:06:37 --> Severity: error --> Exception: Too few arguments to function Templates::public_bootstrap(), 0 passed in C:\xampp\htdocs\cishop\system\core\CodeIgniter.php on line 514 and exactly 1 expected C:\xampp\htdocs\cishop\application\modules\templates\controllers\templates.php 9
+ERROR - 2018-07-17 09:06:37 --> Severity: Error --> Uncaught TypeError: Argument 1 passed to CI_Exceptions::show_exception() must be an instance of Exception, instance of ArgumentCountError given, called in C:\xampp\htdocs\cishop\system\core\Common.php on line 658 and defined in C:\xampp\htdocs\cishop\system\core\Exceptions.php:190
+Stack trace:
+#0 C:\xampp\htdocs\cishop\system\core\Common.php(658): CI_Exceptions->show_exception(Object(ArgumentCountError))
+#1 [internal function]: _exception_handler(Object(ArgumentCountError))
+#2 {main}
+  thrown C:\xampp\htdocs\cishop\system\core\Exceptions.php 190
+ERROR - 2018-07-17 09:07:35 --> Severity: error --> Exception: Too few arguments to function Templates::public_bootstrap(), 0 passed in C:\xampp\htdocs\cishop\application\modules\templates\controllers\templates.php on line 10 and exactly 1 expected C:\xampp\htdocs\cishop\application\modules\templates\controllers\templates.php 13
+ERROR - 2018-07-17 09:07:35 --> Severity: Error --> Uncaught TypeError: Argument 1 passed to CI_Exceptions::show_exception() must be an instance of Exception, instance of ArgumentCountError given, called in C:\xampp\htdocs\cishop\system\core\Common.php on line 658 and defined in C:\xampp\htdocs\cishop\system\core\Exceptions.php:190
+Stack trace:
+#0 C:\xampp\htdocs\cishop\system\core\Common.php(658): CI_Exceptions->show_exception(Object(ArgumentCountError))
+#1 [internal function]: _exception_handler(Object(ArgumentCountError))
+#2 {main}
+  thrown C:\xampp\htdocs\cishop\system\core\Exceptions.php 190
+ERROR - 2018-07-17 09:07:39 --> Severity: error --> Exception: Too few arguments to function Templates::public_bootstrap(), 0 passed in C:\xampp\htdocs\cishop\application\modules\templates\controllers\templates.php on line 10 and exactly 1 expected C:\xampp\htdocs\cishop\application\modules\templates\controllers\templates.php 13
+ERROR - 2018-07-17 09:07:39 --> Severity: Error --> Uncaught TypeError: Argument 1 passed to CI_Exceptions::show_exception() must be an instance of Exception, instance of ArgumentCountError given, called in C:\xampp\htdocs\cishop\system\core\Common.php on line 658 and defined in C:\xampp\htdocs\cishop\system\core\Exceptions.php:190
+Stack trace:
+#0 C:\xampp\htdocs\cishop\system\core\Common.php(658): CI_Exceptions->show_exception(Object(ArgumentCountError))
+#1 [internal function]: _exception_handler(Object(ArgumentCountError))
+#2 {main}
+  thrown C:\xampp\htdocs\cishop\system\core\Exceptions.php 190
+ERROR - 2018-07-17 09:12:13 --> 404 Page Not Found: /index
+ERROR - 2018-07-17 09:18:10 --> 404 Page Not Found: /index
+ERROR - 2018-07-17 09:18:10 --> 404 Page Not Found: /index
+ERROR - 2018-07-17 09:18:10 --> 404 Page Not Found: /index
+ERROR - 2018-07-17 09:19:07 --> 404 Page Not Found: /index
+ERROR - 2018-07-17 09:19:07 --> 404 Page Not Found: /index
+ERROR - 2018-07-17 09:23:55 --> 404 Page Not Found: ../modules/templates/controllers/Templates/bootstrap
---
+++ application/modules/templates/controllers/templates.php
@@ -0,0 +1,25 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Templates extends MX_Controller {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function test() {
+        $this->public_bootstrap(NULL);
+    }
+
+    function public_bootstrap($data) {
+        $this->load->view('public_bootstrap', $data);
+    }
+
+    function public_jqm($data) {
+        $this->load->view('public_jqm', $data);
+    }
+
+    function admin($data) {
+        $this->load->view('admin', $data);
+    }
+
+}
