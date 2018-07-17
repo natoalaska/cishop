28b0326111815f07f2e8daf41a9079eeb7f3f3f7
Nathan Vander Martin <natoalaska@gmail.com>
2018-07-17 08:48:56 -0800

    Default Module
    Added default module that can be copied.

---
+++ application/modules/COPYTHIS/controllers/perfectcontroller.php
@@ -0,0 +1,58 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Perfectcontroller extends MX_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->model = 'mdl_perfectcontroller';
+        $this->load->model($this->model);
+    }
+
+    function get($order_by) {
+        $query = $this->{$this->model}->get($order_by);
+        return $query;
+    }
+
+    function get_with_limit($limit, $offset, $order_by) {
+        $query = $this->{$this->model}->get_with_limit($limit, $offset, $order_by);
+        return $query;
+    }
+
+    function get_where($id) {
+        $query = $this->{$this->model}->get_where($id);
+        return $query;
+    }
+
+    function get_where_custom($col, $value) {
+        $query = $this->{$this->model}->get_where_custom($col, $value);
+        return $query;
+    }
+
+    function _insert($data) {
+        $this->{$this->model}->_insert($data);
+    }
+
+    function _update($id, $data) {
+        $this->{$this->model}->_update($id, $data);
+    }
+
+    function _delete($id) {
+        $this->{$this->model}->_delete($id);
+    }
+
+    function count_where($column, $value) {
+        $count = $this->{$this->model}->count_where($column, $value);
+        return $count;
+    }
+
+    function get_max() {
+        $max_id = $this->{$this->model}->get_max();
+        return $max_id;
+    }
+
+    function _custom_query($mysql_query) {
+        $query = $this->{$this->model}->_custom_query($mysql_query);
+        return $query;
+    }
+
+}
