55e40ab05694240588ef4654cbbec1fbda720613
Nate VanderMartin <natoalaska@gmail.com>
2018-07-17 06:26:56 -0800

    .htaccess and test module
    Added a .htaccess file and a test module

---
+++ application/modules/firsttry/controllers/firsttry.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Firsttry extends MX_Controller {
+
+	function __construct() {
+		parent::__construct();
+	}
+
+	function hello() {
+		echo "Hello";
+	}
+
+}
