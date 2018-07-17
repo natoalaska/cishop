09e8cb7e4e50a53f3becb190165a3ef90bc7ec79
Nathan Vander Martin <natoalaska@gmail.com>
2018-07-17 07:43:58 -0800

    Timezone
    Updated Default Timezone to America/Anchorage

---
+++ application/logs/log-2018-07-17.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2018-07-17 07:43:10 --> Config Class Initialized
+INFO - 2018-07-17 07:43:10 --> Hooks Class Initialized
+DEBUG - 2018-07-17 07:43:10 --> UTF-8 Support Enabled
+INFO - 2018-07-17 07:43:10 --> Utf8 Class Initialized
+INFO - 2018-07-17 07:43:10 --> URI Class Initialized
+INFO - 2018-07-17 07:43:10 --> Router Class Initialized
+INFO - 2018-07-17 07:43:10 --> Output Class Initialized
+INFO - 2018-07-17 07:43:10 --> Security Class Initialized
+DEBUG - 2018-07-17 07:43:10 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-17 07:43:10 --> Input Class Initialized
+INFO - 2018-07-17 07:43:10 --> Language Class Initialized
+INFO - 2018-07-17 07:43:10 --> Language Class Initialized
+INFO - 2018-07-17 07:43:10 --> Config Class Initialized
+INFO - 2018-07-17 07:43:10 --> Loader Class Initialized
+INFO - 2018-07-17 07:43:10 --> Controller Class Initialized
+DEBUG - 2018-07-17 07:43:10 --> Firsttry MX_Controller Initialized
+INFO - 2018-07-17 07:43:10 --> Final output sent to browser
+DEBUG - 2018-07-17 07:43:10 --> Total execution time: 0.0340
